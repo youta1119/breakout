d6b0b76c9d7059b39769ba8b6f7a530a481d2f43
youta ogino <youta1119ume@gmail.com>
2016-02-24 00:11:38 +0900

    fix ball object

--- breakout/app/src/main/java/applicatipn/android/breakout/Ball.kt
+++ breakout/app/src/main/java/applicatipn/android/breakout/Ball.kt
@@ -4,21 +4,14 @@ package applicatipn.android.breakout
  * Created by Youta on 2016/02/20.
  */
 class Ball(x:Float,y:Float,w:Float,h:Float){
-    var SpeedX:Int=20
-    var SpeedY:Int=20
-    val size:Float =30f
-    var x:Float = 0f
-    var y:Float=0f
-    var h:Float=0f
-    var w:Float=0f
+    var SpeedX=20;
+    var SpeedY=20;
+    val size=30f;
+    var x =x;
+    var y=y;
+    val h=h;
+    val w=w;
 
-
-    init {
-        this.x=x;
-        this.y=y;
-        this.h=h;
-        this.w=w;
-    }
     fun move(){
         if(x>=w-size||x<=0){
             SpeedX=-SpeedX;
